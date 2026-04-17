45103c9667aaed766044270bfb542c8ed79b270c
Kevin Dadonas <kdadonas@gmail.com>
2026-04-16 22:09:43 -0300

    16/04/2026 commit

--- exercícios/script.js
+++ exercícios/script.js
@@ -3,3 +3,44 @@ function churras(){
     let kgs = (qtedePessoas * 400) / 1000 //transforma gramas em kilos
     document.getElementById('resultadoChurras').innerText = "Total de carne " + kgs + " Kg"
 }
+
+function febre(){
+    //obter a temperatura digitada pelo usuário
+    let temperatura = Number(document.getElementById('temperatura').value)
+    //verificar o estado febril
+    if(temperatura >= 37.5){
+        //mostrar o resultado de estado febril ao usuário
+        document.getElementById('resultadoFebril').innerText = "Está com febre"
+    }
+    else{
+        document.getElementById('resultadoFebril').innerText = "Temperatura normal"   
+    }
+}
+
+function credito(){
+    let renda = Number(document.getElementById('renda').value)
+    let idade = Number(document.getElementById('idade').value)
+
+    if(renda >= 2000 && idade >= 18){ //operador && significa AND (ou E)
+        document.getElementById('resultadoCredito').innerText = "Crédito concedido"
+    }
+    else{
+        document.getElementById('resultadoCredito').innerText = "Crédito negado"
+    }
+    //operador || significa OR (ou OU)
+}
+
+function luta(){
+    let idade = Number(document.getElementById('idadeLuta').value)
+    let peso = Number(document.getElementById('peso').value)
+
+    if(idade < 18){ //operador && significa AND (ou E)
+        document.getElementById('resultadoLuta').innerText = "Categoria juvenil"
+    }
+    else if(peso < 80){ //idade >=18
+        document.getElementById('resultadoLuta').innerText = "Categoria adulto - Subcategoria leve"
+    }
+    else{ //peso >= 80
+        document.getElementById('resultadoLuta').innerText = "Categoria adulto - Subcategoria pesado"
+    }
+}
